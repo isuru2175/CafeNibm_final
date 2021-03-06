14c192e20e1abc0d7af30ebf2952d58df66a11f5
isuru <isuru2175@gmail.com>
2021-03-06 02:02:41 -0800

    add ui

--- CafeNibm/ViewController.swift
+++ CafeNibm/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class ViewController: UIViewController {
-
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    let auth = Auth.auth()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onLogin(_ sender: UIButton) {
+//        auth.signIn(withEmail: email.text!,password: password.text!, completion: {result,err in
+//            //result.
+//            if err == nil{
+//                 self.performSegue(withIdentifier: "Home", sender: self)
+//            }
+//            var message:String,title:String
+//
+//            switch AuthErrorCode(rawValue: err!._code){
+//            case .networkError:
+//                title="Network Error"
+//                message="check your Connection"
+//
+//            default:
+//                title="Somthing Wrong"
+//                message="check your Connection"
+//
+//
+//            }
+//            var  alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//            self.present(alert, animated: true)
+//        })
+       if let email = email.text, let password = password.text
+       {
+           Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+             
+               if let errr=error
+               {
+                   print(errr)
+               }
+               else
+               {
+                   self.performSegue(withIdentifier: "Home", sender: self)
+               }
+           }
+       }
+    }
+    
+    
+    @IBAction func Register(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "Register", sender: self)
+    }
+    
+    
 }
 
